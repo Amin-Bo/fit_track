c6f2231c5cf8048fc6eecc871601d7e87b195a1a
AminBenOthmen <aminos.bo.12@gmail.com>
2025-03-17 01:38:43 +0100

    remove import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:fit_track/authScreens/LoginScreen.dart';
 import 'package:fit_track/authScreens/RegisterScreen.dart';
-import 'package:fit_track/authScreens/SplashScreen.dart';
 import 'package:fit_track/firebase_options.dart';
 import 'package:flutter/material.dart';
 
